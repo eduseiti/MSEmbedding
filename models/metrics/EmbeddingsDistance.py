453c6a618ef8f3be04b0cc3d2147795ebc939949
Eduardo Seiti de Oliveira <eduseiti@gmail.com>
2019-07-19 09:06:53 -0300

    Initial tests with MedR implementation

--- models/metrics/EmbeddingsDistance.py
+++ models/metrics/EmbeddingsDistance.py
@@ -4,6 +4,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from bootstrap.lib.options import Options
 from bootstrap.lib.logger import Logger
+from scipy.spatial.distance import cdist
 
 class EmbeddingsDistance(torch.nn.Module):
 
@@ -13,34 +14,53 @@ class EmbeddingsDistance(torch.nn.Module):
 
     def forward(self, criterionOutput, networkOutput, batch):
 
-        validationResult = np.zeros((len(batch) // 3 * 2, 2), dtype = bool)
+        embeddings = networkOutput.cpu()
+        embeddings = embeddings.numpy()
+
+        print('Dimensions: {}'.format(embeddings.shape))
+
+        ranks = []
+        ranksTorch = []
+
+        # validationResult = np.zeros((len(batch) // 3 * 2, 2), dtype = bool)
 
         for i in range(len(networkOutput) // 3):
 
-            positiveDistance = torch.dist(networkOutput[i * 3], networkOutput[i * 3 + 1])
-            negativeDistance = torch.dist(networkOutput[i * 3], networkOutput[i * 3 + 2])
+            distances = cdist(embeddings[i * 3].reshape(1, -1), embeddings.reshape(embeddings.shape[0], -1))
 
-            Logger()('{} - Positive distance={}, Negative distance={}'.format(i, positiveDistance, negativeDistance))
+            netOutputFixed = networkOutput.contiguous()
 
-            lossMargin = Options()['model']['criterion']['loss_margin']
+            distancesTorch = torch.cdist(netOutputFixed[i * 3].view(1, -1), netOutputFixed.view(netOutputFixed.shape[0], -1))
 
-            if torch.lt(positiveDistance, lossMargin):
-                validationResult[i * 2] = (True, True)
-            else:
-                validationResult[i * 2] = (True, False)
-            
-            if torch.lt(negativeDistance, lossMargin):
-                validationResult[i * 2 + 1] = (True, False)
-            else:
-                validationResult[i * 2 + 1] = (True, True)
+            orderedDistances = np.argsort(distances)
+
+            orderedDistancesTorch = np.argsort(distancesTorch.cpu())
+
+            #
+            # Keep the rank of the positive example of the given anchor - it should be the nearest point.
+            # Since the anchor itself is still in the list, and the distance to it will always be zero, decrement the
+            # positive example ranking.
+            #
+
+            positiveExampleRank = orderedDistances[0].tolist().index(i * 3 + 1) - 1
+            negativeExampleRank = orderedDistances[0].tolist().index(i * 3 + 2) - 1
+
+            postiveExampleRankTorch = orderedDistancesTorch[0].tolist().index(i * 3 + 1) - 1
+
+            ranks.append(positiveExampleRank)
+            ranksTorch.append(postiveExampleRankTorch)
+
+            Logger()('{} - Positive rank={}, Positive rank torch={} Negative rank={}'.format(i, positiveExampleRank, postiveExampleRankTorch, negativeExampleRank))
 
 
         out = {}
 
-        totalAccuracyScore = accuracy_score(validationResult[:, 0], validationResult[:, 1])
+        MedR = np.mean(ranks)
+        MedR_torch = np.mean(ranksTorch)
 
-        print('Validation accuracy: {}'.format(totalAccuracyScore))
+        print('Validation MedR={}, MedR_torch={}'.format(MedR, MedR_torch))
 
-        out['accuracy'] = totalAccuracyScore
+        out['MedR'] = MedR
+        out['MedR_torch'] = MedR_torch
 
         return out
