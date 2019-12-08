99fa35c780167dcaeb4dbe55eb76f7f13de58482
Eduardo Seiti de Oliveira <eduseiti@gmail.com>
2019-12-08 19:25:44 -0300

    Trying agglomerative

--- clustering.py
+++ clustering.py
@@ -5,6 +5,7 @@ import torch
 import os
 
 from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
 
 EMBEDDINGS_FILE = "spectra_embeddings_018222.pkl"
 EMBEDDINGS_FOLDER = "data/linfeng"
@@ -25,10 +26,18 @@ all_embeddings = torch.cat(all_embeddings).numpy()
 
 print("all_embeddings.shape: {}".format(all_embeddings.shape))
 
-kmeans_result = KMeans(n_clusters=CLUSTERS_NUMBER, random_state=0).fit(all_embeddings)
+# kmeans_result = KMeans(n_clusters=CLUSTERS_NUMBER, random_state=0).fit(all_embeddings)
 
-print("cluster centers: {}".format(kmeans_result.cluster_centers_))
-print("clustered labels: {}".format(kmeans_result.labels_))
+# print("cluster centers: {}".format(kmeans_result.cluster_centers_))
+# print("clustered labels: {}".format(kmeans_result.labels_))
+
+# with open(os.path.join(EMBEDDINGS_FOLDER, KMEANS_RESULTS_FILE), "wb") as outputFile:
+#     pickle.dump(kmeans_result, outputFile, pickle.HIGHEST_PROTOCOL)
+
+agglomerative_result = AgglomerativeClustering(n_clusters=None, affinity="cosine", linkage="complete", distance_threshold=1e-10).fit(all_embeddings)
+
+print("clustered labels: {}".format(agglomerative_result.labels_))
 
 with open(os.path.join(EMBEDDINGS_FOLDER, KMEANS_RESULTS_FILE), "wb") as outputFile:
-    pickle.dump(kmeans_result, outputFile, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(agglomerative_result, outputFile, pickle.HIGHEST_PROTOCOL)
+
