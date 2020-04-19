dce78da08a593fb078e675cf0dc0ac5aee565a5a
Eduardo Seiti de Oliveira <eduseiti@gmail.com>
2020-04-18 21:24:33 -0300

    Several changes to create the pvalues for the entire PXD000561 list of experiments

--- scripts/count_spectra.sh
+++ scripts/count_spectra.sh
@@ -3,16 +3,16 @@
 #
 # Output a tab-separated list of .mgf filenames and the number of spectra within it.
 #
-# ./count_spectra.sh <folder-where-mgf-files-are>
+# ./count_spectra.sh <file-with-the-list-of-all-mgf-files>
 #
 
-EXPERIMENTS_FILES=($(ls $1/*.mgf))
+EXPERIMENTS_FILES=($(cat $1))
 
 echo -e "file\tspectra count"
 
 for file in "${EXPERIMENTS_FILES[@]}"; do
-    spectra_count=$(grep "BEGIN IONS" $file | wc -l)
 
+    spectra_count=$(grep "BEGIN IONS" $file | wc -l)
     only_filename=$(echo $file | sed -r "s/(.+\/)?(.+)$/\2/")
 
     echo -e "$only_filename\t$spectra_count"
